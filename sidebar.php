cebc332685eaeeccd98a3e9e057b756cdf7e1e0e
Vojislav Stupar <vojislav.stupar@gmail.com>
2018-12-20 20:01:11 +0100

    zavrsen 7 zadatak

--- sidebar.php
+++ sidebar.php
@@ -1,31 +1,58 @@
+<?php
+    // ako su mysql username/password i ime baze na vasim racunarima drugaciji
+    // obavezno ih ovde zamenite
+    $servername = "127.0.0.1";
+    $username = "root";
+    $password = "vivify";
+    $dbname = "blog_zavrsni";
+
+    try {
+        $connection = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        // set the PDO error mode to exception
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch(PDOException $e)
+    {
+        echo $e->getMessage();
+    }
+?>
+
+          
 <aside class="col-sm-3 ml-sm-auto blog-sidebar">
             <div class="sidebar-module sidebar-module-inset">
-                <h4>About</h4>
-                <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-            </div>
-            <div class="sidebar-module">
-                <h4>Archives</h4>
-                <ol class="list-unstyled">
-                    <li><a href="#">March 2014</a></li>
-                    <li><a href="#">February 2014</a></li>
-                    <li><a href="#">January 2014</a></li>
-                    <li><a href="#">December 2013</a></li>
-                    <li><a href="#">November 2013</a></li>
-                    <li><a href="#">October 2013</a></li>
-                    <li><a href="#">September 2013</a></li>
-                    <li><a href="#">August 2013</a></li>
-                    <li><a href="#">July 2013</a></li>
-                    <li><a href="#">June 2013</a></li>
-                    <li><a href="#">May 2013</a></li>
-                    <li><a href="#">April 2013</a></li>
-                </ol>
-            </div>
-            <div class="sidebar-module">
-                <h4>Elsewhere</h4>
-                <ol class="list-unstyled">
-                    <li><a href="#">GitHub</a></li>
-                    <li><a href="#">Twitter</a></li>
-                    <li><a href="#">Facebook</a></li>
-                </ol>
+                <h4>Latest posts</h4>
+                <?php
+              // pripremamo upit
+              $sql1 = "SELECT id,title FROM posts ORDER BY created_at";
+              $statement = $connection->prepare($sql1);
+
+              // izvrsavamo upit
+              $statement->execute();
+
+              // zelimo da se rezultat vrati kao asocijativni niz.
+              // ukoliko izostavimo ovu liniju, vratice nam se obican, numerisan niz
+              $statement->setFetchMode(PDO::FETCH_ASSOC);
+
+              // punimo promenjivu sa rezultatom upita
+              $posts_title = $statement->fetchAll();
+
+              // koristite var_dump kada god treba da proverite sadrzaj neke promenjive
+                //   echo '<pre>';
+                //   var_dump($posts_title);
+                //   echo '</pre>';
+            ?>
+            <ul>
+                <?php
+                    foreach($posts_title as $title) {
+                ?>
+                <a href="single-post.php?post_id=<?php echo($title['id'])?>"><li>
+                        <?php
+                            echo $title['title'];
+                        ?>
+                </a></li>
+                <?php
+                    }
+                ?>
+            </ul>
             </div>
 </aside><!-- /.blog-sidebar -->
